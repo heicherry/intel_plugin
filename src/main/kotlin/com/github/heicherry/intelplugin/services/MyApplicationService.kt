2285ed5c705df5365922f335aaa4e7f27e85f0ec
GitHub Action <action@github.com>
2022-01-04 05:56:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/heicherry/intelplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/heicherry/intelplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.heicherry.intelplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.heicherry.intelplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/heicherry/intelplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/heicherry/intelplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.heicherry.intelplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.heicherry.intelplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
